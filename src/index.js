9b8d26a510fdf1541994d96d1095874962758913
phamdongchinh683 <phamdongchinh123789@gmail.com>
2024-08-31 09:19:42 +0700

    hello world

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-const express = require("express");
+import express from "express";
+import "dotenv/config";
+
 const app = express();
-let dotenv = require("dotenv").config();
 
 const port = process.env.PORT;
 
