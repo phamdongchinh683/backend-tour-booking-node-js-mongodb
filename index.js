458ca3f0bf6ab945212abbd13c6f03505370ef33
phamdongchinh683 <phamdongchinh123789@gmail.com>
2024-11-18 01:03:51 +0700

    update cors client 18-11-2024

--- index.js
+++ index.js
@@ -6,17 +6,23 @@ const morgan = require("morgan");
 const port = process.env.PORT;
 const router = require("./src/routers/router");
 const app = express();
+const corsOptions = require("./src/config/corsOptions");
 
 const START_SERVER = () => {
-  app.use(cors());
+  app.use(cors(corsOptions));
   app.use(bodyParser.json());
   app.use(express.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(morgan("combined"));
   router(app);
-  app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-  });
+
+  if (process.env.BUILD_MODE === "production") {
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `Production: Hi ${process.env.AUTHOR}, Back-end Server is running successfully at Port: ${process.env.PORT}`
+      );
+    });
+  }
 };
 
 mongodb();
