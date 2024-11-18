2d98c35af577c585e6593fb6456a10add20525ae
phamdongchinh683 <phamdongchinh123789@gmail.com>
2024-11-18 21:47:40 +0700

    update 18-11-2024

--- src/routers/router.js
+++ src/routers/router.js
@@ -3,8 +3,8 @@ const routerAdmin = require("./Admin");
 function router(app) {
   app.use("/api/v1/auth", routerAuth);
   app.use("/api/v1/admin", routerAdmin);
-  app.get("/", () => {
-    res.send("Hello world");
+  app.get("/", (req, res) => {
+    res.json({ message: "Hello world" });
   });
 }
 
