0843df186f7b84f650ebf35c846a69f78ed9cb4b
phamdongchinh683 <phamdongchinh123789@gmail.com>
2024-11-18 21:39:09 +0700

    update

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const mongodb = require("./src/config");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const swaggerUi = require("swagger-ui-express");
-const swaggerJsdoc = require("swagger-jsdoc");
 const router = require("./src/routers/router");
 const app = express();
 const corsOptions = require("./src/config/corsOptions");
-const options = require("./src/config/swagger");
+const PORT = process.env.PORT || 3000;
 
 const START_SERVER = () => {
   app.use(cors(corsOptions));
@@ -16,15 +14,13 @@ const START_SERVER = () => {
   app.use(express.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(morgan("combined"));
-  // const specs = swaggerJsdoc(options);
 
-  // app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
   router(app);
 
   if (process.env.BUILD_MODE === "production") {
-    app.listen(process.env.PORT, () => {
+    app.listen(PORT, () => {
       console.log(
-        `Production: Hi ${process.env.AUTHOR}, Back-end Server is running successfully at Port: ${process.env.PORT}`
+        `Production: Hi ${process.env.AUTHOR}, Back-end Server is running successfully at Port: ${PORT}`
       );
     });
   }
