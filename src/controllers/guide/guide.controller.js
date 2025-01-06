165dcb0484bcade818c68a8e26bf348ac224335b
Chinh Chinh <128126308+phamdongchinh683@users.noreply.github.com>
2025-01-06 23:46:34 +0700

    Delete src/controllers/guide directory

--- src/controllers/guide/guide.controller.js
+++
@@ -1,7 +0,0 @@
-const { responseStatus } = require("../../globals/handler");
-const UserService = require("../../services/auth/user.service");
-class GuideController {
-  // guide
-}
-
-module.exports = new GuideController();
